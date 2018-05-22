c4a020e3274999bf70bdac5b94e9463c65ab659b
Aisha Saeed <aisha.saeed@aalto.fi>
2018-05-23 02:43:08 +0300

    Correcting bugs related to token and email handling

--- omi_security/security_node/views.py
+++ omi_security/security_node/views.py
@@ -16,23 +16,20 @@ import json
 import jwt
 
 
-@login_required                                                             #check first if user is logged in or not
+@login_required                                                                                           #check first if user is logged in or not
 def home(request):
-    response = render(request, "home.html")                                 #checks and prints if user is superuser or a normal user
-    token = jwt.encode({'email': request.user.email, 'is_superuser': request.user.is_superuser}, 'MySecretKey', algorithm='HS256').decode('utf-8') #The decode call here doesn't decode the jwt, it converts the encoded jwt from a byte string to a utf-8 string.
-    #token = jwt.encode({'email': request.user.email, 'is_superuser':request.user.is_superuser}, 'MySecretKey', algorithm='HS256')       #Creates jwt token
-    #response.set_cookie("email", request.user.email)
-    response.set_cookie("token", token)                                     #use the token in session cookie
-    return response                                                         #return cookie to client/user
+    response = render(request, "home.html")                                                               #checks and prints if user is superuser or a normal user
+    token = jwt.encode({'email': request.user.email, 'is_superuser': request.user.is_superuser}, 'MySecretKey', algorithm='HS256').decode('utf-8') #Creates jwt token. The decode call here doesn't decode the jwt, it converts the encoded jwt from a byte string to a utf-8 string.
+    response.set_cookie("token", token)                                                                   #use the token in session cookie
+    return response                                                                                       #return cookie to client/user
 
 
 @login_required
-def about(request):                                                         #now when user tries to access/request the "about" page, the token is send by client
+def about(request):                                                                                       #now when user tries to access/request the "about" page, the token is send by client
     token = request.COOKIES.get('token')
     print(token)
-    token = jwt.decode(token, 'MySecretKey', algorithm=['HS256'])            #decode the token
-    #token = jwt.decode(eval(token), 'MySecretKey', algorithm=['HS256'])
-    return render(request, "about.html",{'token':token})                    #send the decoded token to about page
+    token_decoded = jwt.decode(token, 'MySecretKey', algorithm=['HS256'])                                 #decode the token
+    return render(request, "about.html",{'token_decoded':token_decoded,'token':token})                    #send the decoded token to about page
 
 
 
@@ -52,18 +49,11 @@ def authmodule(request):
                 else:
                     message = form.errors['group_name'].as_text()           #if validity fails, get error message as text form and save in message variable
                 group_added_id = Group.objects.get(group_name=request.POST["group_name"])   #gets group id of the group just added
-                #print('11111111111', group_added_id)
                 for user in users_added:
                     instance = User_Group_Relation()                        #get User_Group_Relation table
-                    #instance.user_id = Registered_Users.objects.get(id=int(user))
                     instance.user_id = User.objects.get(id=int(user))       #get user id in User_Group_Relation table by matching user id of User table and in users-added list of user ids
                     instance.group_id = group_added_id                      #get group id in User_Group_Relation table
                     instance.save()                                         #save values in table
-            #elif action == 'adduser':
-                #form = Registered_UsersForm(request.POST)
-                #if form.is_valid():
-                    #form.save()
-        #registered_users = Registered_Users.objects.all()
         users = User.objects.filter(is_superuser=False)                     #get all users who are not super users
         registered_groups = Group.objects.all()                             #get all groups
         return render(request, "authmodule.html",{"list_users":users, "list_groups":registered_groups, 'errormessage':message })    #send users, groups and error message(if any) to authmodule.html file
@@ -115,28 +105,30 @@ def signup(request):                                                 #function u
 
 
 def omi_authquery(request):
-    email = request.GET.get('email')
-    token = request.COOKIES.get('token')
-    print('--------------',token)
-    #try:
-    status=False                                                #set a variable "status" to false initially
-    user = User.objects.get(email=email)                        #get the user from User table by matching with requested email address
-    if user.is_superuser:                                       #if he is a super user then status is changed to true
-        status=True
-    decoded_token = jwt.decode(token, 'MySecretKey', algorithm=['HS256'])
-    decoded_email = decoded_token['email']
-    print(decoded_token, '------********------', decoded_email)
-    users_id = User.objects.get(email=email).pk                                              #gets user id from the User  table after matching with requested email address
-    relation_group_id = User_Group_Relation.objects.filter(user_id=int(users_id))            #gets group id of in which that user belongs
-    for r in relation_group_id:
-        print('555555',r.group_id.id)
-    reply = json.dumps({'email': email, 'userExist': True, 'isAdmin':status})                #making json response if user is in a group: user exists status, admin status and his email address
-    #except:
-        #reply=json.dumps({'email':email, 'userExist':False, 'isAdmin':status})              #making json response if user is not in a group: user exists status, admin status and his email address
-    #{'allow': [<paths>], 'deny': [<paths>], 'isAdmin': true|false}
+    '''
+    What this function do
+    :param request: request from browser
+    :return: returns the details of user or return error if token is invalid or email address doesnot exists in database
+    '''
+    email = request.GET.get('email')                                                    #get the email address from URL
+    token = request.GET.get('token')                                                    #get the token from URL
+    if token:                                                                           #in case token is provided
+        try:
+            decoded_token = jwt.decode(token, 'MySecretKey', algorithm=['HS256'])       #decode the jwt token
+            decoded_email = decoded_token['email']                                      #get email from token
+            user = User.objects.get(email=decoded_email)                                # get the user from User table by matching with decoded email address
+        except:
+            reply = json.dumps({'message': 'Invalid Token or No user Exists'})          #if invalid token is provided, sends error message
+            return HttpResponse(reply)
+    elif email:                                                                         #in case email is provided
+        try:
+            user = User.objects.get(email=email)                                        # get the user from User table by matching with requested email address
+        except:
+            reply = json.dumps({'message': 'No User Exist with this email address'})    # if invalid email is provided, sends error message
+            return HttpResponse(reply)
+    reply = json.dumps({'email': user.email, 'userExist': True, 'isAdmin':user.is_superuser})   #making json response: user email address, user exists status, his admin status
     return HttpResponse(reply)
-
-
+    #{'allow': [<paths>], 'deny': [<paths>], 'isAdmin': true|false}
 
 
 
