6d2c805e072b7de8ba2ec5a7b2d5229876c859cf
Sankalp <Deadlyelder@users.noreply.github.com>
2018-06-25 08:45:03 +0200

    Update install.sh
    Added all dependencies

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-sudo apt-get install python3-pip
+sudo apt-get install python3 python3-pip libsasl2-dev python-dev libldap2-dev libssl-dev python-ldap
 pip3 install -r requirements.txt
-
-
-
